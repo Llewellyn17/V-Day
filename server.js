3b9504a9bc1c470c290b275b26e1066a95c9012d
llewellyn17 <llewellyn.isaac@yahoo.com>
2019-03-18 17:10:36 -0500

    why

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
-const fs = require ('fs')
 
 const app = express();
-const port = '3000';
+const port = '3001';
 
-
-app.use(bodyParser.urlencoded({ extended: false}));
-
-
-
-
-app.post('/scores', (req, res) => {
-    let name = req.body.name;
-    let score = req.body.score;
-    let item = {"name":name, "score":score};
-    fs.appendFileSync(dataPath,
-        JSON.stringify(item, null, 4), 'utf-8', err => console.log(err));
-        res.redirect('/');
-
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/looks/index.html');
 });
 
-
-app.use(express.static(path.join(__dirname,'looks')));
-
-app.listen(port,function(){
+app.use(express.static(path.join(__dirname + '/')));
+app.listen(process.env.PORT || 5000, function () {
 });
